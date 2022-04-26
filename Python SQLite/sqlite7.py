198d1e9b75e60032b943dee0c96c6ac859a930c0
DarknessKiller <23330152+DarknessKiller@users.noreply.github.com>
2022-04-26 17:25:49 +0800

    Python SQLite/sqlite7.py: Update My Noob Coding...

--- Python SQLite/sqlite7.py
+++ Python SQLite/sqlite7.py
@@ -5,17 +5,21 @@ def main():
 	#conn = sqlite3.connect('contacts.db')
 	conn = sqlite3.connect("chocolate.db") # if you want to point to a different location 'conn = sqlite3.connect(r"C:\Databases\chocolate.db")'
 	cur = conn.cursor()
-	cur2 = conn.cursor()
-	cur3 = conn.cursor()
 
 	product_id = int(input('Enter a product ID: '))
-	cur.execute('SELECT Description FROM Products WHERE ProductID = ?', (product_id,))
-	cur2.execute('SELECT RetailPrice FROM Products WHERE ProductID = ?', (product_id,))
-	print("The current price for", cur.fetchall(), 'is', cur2.fetchall())
-	new_price = (input('Enter the new price: '))
-	cur3.execute(''' UPDATE Products SET RetailPrice = ? WHERE ProductID = ? ''', (new_price, product_id))
+	cur.execute('SELECT Description, RetailPrice FROM Products WHERE ProductID = ?', (product_id,))
+	result = cur.fetchone()
 
-	conn.commit()
+	if result != None:
+		print(f"The current price for {result[0]} is ${result[1]:.2f}")
+		new_price = (input('Enter the new price: '))
+		cur.execute('''UPDATE Products SET RetailPrice = ?
+			           WHERE ProductID == ?''',
+			           (new_price, product_id))
+		conn.commit()
+		print(f"The price for {result[0]} has been updated.")
+	else:
+		print(f"The Product ID '{product_id}' is not found.")
 	conn.close()
 
 
